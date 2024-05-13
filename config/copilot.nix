41e0b9028112a238343c96c01e679cca12c392c4
mood <74420740+no-mood@users.noreply.github.com>
2024-05-13 11:39:23 +0200

    WIP

---
+++ config/copilot.nix
@@ -0,0 +1,9 @@
+{
+
+plugins = {
+  copilot-cmp = {
+    # enable = true;
+  };
+
+};
+}
--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,19 @@
 {
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+  imports = [ 
+  ./bufferline.nix
+./colorscheme.nix
+./copilot.nix
+  ];
+
+
+  viAlias = true;
+  vimAlias = true;
+
+  plugins = {
+    alpha = {
+      enable = true;
+      theme = "dashboard";
+    };
+  };
 }
